161306df5d10c481eea41efff20d0a1ff8c8e979
WiSeoungHwan <dnltjdghks1@gmail.com>
2019-09-11 17:05:28 +0900

    Straight effect

--- Game/Bullet/WoodStraight.cs
+++ Game/Bullet/WoodStraight.cs
@@ -9,6 +9,20 @@ namespace MyIsland
         #region Private Field
         private bool isShoot;
         #endregion
+
+        #region Collider
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                isShoot = false;
+                EventManager.Instance.emit(EVENT_TYPE.TILE_HIT,this,TargetTile);
+                Effect.Play();
+                StartCoroutine("PositionReset");
+                
+            }
+        }
+        #endregion
         #region Public Methods
         public override void Fire()
         {
@@ -25,7 +39,7 @@ namespace MyIsland
         {
             if (this.TargetTile.tileData.isPlayerGround)
             {
-                transform.Translate(new Vector3(this.TargetTile.transform.position.x * 2f * Time.deltaTime, 0f, 0f), Space.World);
+                transform.Translate(new Vector3(this.TargetTile.transform.position.x * 4f * Time.deltaTime, 0f, 0f), Space.World);
                 if (this.transform.position.x >= this.TargetTile.transform.position.x + 5f)
                 {
                     isShoot = false;
@@ -33,15 +47,15 @@ namespace MyIsland
                     // {
                     //     i.TileTargeting(false);
                     // }
-                    this.transform.localPosition = new Vector3(0f,0.5f,0);
+                    this.transform.localPosition = new Vector3(0f, 0.5f, 0);
                     Effect.gameObject.SetActive(false);
                     Effect.Stop();
-                   
+
                 }
             }
             else
             {
-                transform.Translate(new Vector3(this.TargetTile.transform.position.x * 1f * Time.deltaTime, 0f, 0f), Space.World);
+                transform.Translate(new Vector3(this.TargetTile.transform.position.x * 4f * Time.deltaTime, 0f, 0f), Space.World);
                 if (this.transform.position.x <= this.TargetTile.transform.position.x - 5f)
                 {
 
@@ -50,7 +64,7 @@ namespace MyIsland
                     // {
                     //     i.TileTargeting(false);
                     // }
-                    this.transform.localPosition = new Vector3(0f,0.5f,0);
+                    this.transform.localPosition = new Vector3(0f, 0.5f, 0);
                     Effect.gameObject.SetActive(false);
                     Effect.Stop();
                 }
@@ -60,12 +74,14 @@ namespace MyIsland
         IEnumerator PositionReset()
         {
             yield return new WaitForSeconds(2.0f);
-            
+            this.transform.localPosition = new Vector3(0f, 0.5f, 0);
+            Effect.gameObject.SetActive(false);
+            Effect.Stop();
         }
 
         #endregion
 
-        #region MonoBehaviour
+        #region MonoBehaviour Callback
         void Update()
         {
             if (isShoot)
